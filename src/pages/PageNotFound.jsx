1104c47ffedb2c0343207e2f25dc54fc0a45baf0
Shubham Kamble <shubhamkamble3011@gmail.com>
2026-02-26 10:43:15 +0530

    fav

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PageNotFound = () => {
   return (
     <div>
-      <h1>Page Not Found!</h1>
+      <h1 className='text-9xl mt-10 text-center text-red-400'>Page Not Found</h1>
     </div>
   )
 }
--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -1,78 +1,9 @@
-// const SingleRecipe = () => {
-//     const params = useParams()
-//     const [data] = useContext(recipecontext)
-
-//     const rcipe = data.find((recipe) => params.id == recipe.id)
-     
-//      const { register, handleSubmit, reset, formState: { errors } } = useForm();
-//     const [recipes, Setrecipe] = useContext(recipecontext);
-
-//     const navigate=useNavigate()
-
-//     useParams()
-//     const rec=data.find((recipe)=>params.id==recipe.id)
-
-//     const SubmitHandler = (recipe) => {
-//         const index=data.findIndex((recipe)=>params.id==recipe.id);
-//         const copydata=[...data];
-//         copydata[index]={...copydata[index],...recipe}
-//         Setrecipe(copydata)
-        
-//     }
-
-//     const handleDelete=()=>{
-//         const filterdata=data.find((recipe)=>params.id==recipe.id);
-//         Setrecipe(filterdata);
-//         toast.success("recipe deleted!")
-//         navigate("/recipes")
-//     }
-//     return (
-//         recipe ? <div className='w-full flex'>
-//             <div className="left w-1/2 p-2">
-//                 <h1 className='text-5xl font-black'>{rcipe.title}</h1>
-//                 <img className='h-[20vh]' src={rcipe.image} alt="" />
-//             </div>
-
-//             <form className='1/2 p-2' onSubmit={handleSubmit(SubmitHandler)}>
-//                 <input type="url" className='block border-b outline-0  p-2'  {...register("image")} placeholder='image url' value={rcipe.image} />
-//                 <small className='text-red-400'>abc</small>
-
-//                 <input type="text" className='block border-b outline-0  p-2' placeholder='Recipe Title' {...register("title")} value={rcipe.title} />
-
-//                 <input type="text" className='block border-b outline-0  p-2' placeholder='Chef name' {...register("chef")} />
-
-
-//                 <textarea name="" className='block border-b outline-0  p-2' placeholder='enter description' {...register("desc")} value={rcipe.chef} > </textarea>
-
-
-//                 <textarea name="" className='block border-b outline-0  p-2' placeholder='ingredients' {...register("ingr")}  ></textarea>
-
-
-//                 <textarea name="" className='block border-b outline-0  p-2' placeholder='instructions' {...register("ins")} ></textarea>
-
-//                 <select name="" className='block border-b outline-0  p-2'  {...register("category")} >
-//                     <option value="breakfast">Breakfast</option>
-//                     <option value="lunch">Lunch</option>
-//                     <option value="dinner">Dinner</option>
-//                 </select>
-
-
-//                 <button className='block mt-5 bg-blue-900 px-3 py-2 rounded cursor-pointer'>Update Recipe</button>
-//                 <button className='block mt-5 bg-red-500 px-3 py-2 rounded cursor-pointer' onClick={handleDelete}>Delete</button>
-
-
-//             </form>
-
-
-
-//         </div> : "Loading...."
-//     )
-// }
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router'
 import { recipecontext } from '../context/RecipeContext'
 import { useForm } from 'react-hook-form'
 import { toast } from 'react-toastify';
+
 
 const SingleRecipe = () => {
 
@@ -84,12 +15,12 @@ const SingleRecipe = () => {
     const recipe = data.find((r) => params.id == r.id);
 
     const { register, handleSubmit } = useForm({
-        title:data?.title,
-        chef:data?.chef,
-        image:data?.image,
-        ins:data?.ins,
-        desc:data?.desc,
-        ingr:data?.ingr
+        title: data?.title,
+        chef: data?.chef,
+        image: data?.image,
+        ins: data?.ins,
+        desc: data?.desc,
+        ingr: data?.ingr
     });
 
     const UpdateHandler = (updatedRecipe) => {
@@ -97,29 +28,56 @@ const SingleRecipe = () => {
         const copydata = [...data];
         copydata[index] = { ...copydata[index], ...updatedRecipe };
         Setrecipe(copydata);
-         localStorage.setItem("recipes",JSON.stringify(copydata))
+        localStorage.setItem("recipes", JSON.stringify(copydata))
         toast.success("Recipe updated!");
     }
 
     const handleDelete = () => {
         const filterdata = data.filter((r) => params.id != r.id);
         Setrecipe(filterdata);
-         localStorage.setItem("recipes",JSON.stringify(filterdata))
+        localStorage.setItem("recipes", JSON.stringify(filterdata))
         toast.success("Recipe deleted!");
         navigate("/recipes");
     }
- 
 
-    useEffect(()=>{
-        console.log("SingleRecipe.jsx Mounted");
-        return ()=>{
-           console.log("SingleRecipe.jsx Unmounted");    
+
+   
+
+    const[favorite,Setfavorite]=useState(
+        JSON.parse(localStorage.getItem("fav")) || []
+    )
+
+    
+  const handleFav = () => {
+    const copydata=[...favorite];
+    copydata.push(recipe)
+    Setfavorite(copydata)
+        favorite.push(recipe)
+       localStorage.setItem("fav",JSON.stringify(favorite))
+    }
+
+    const handleUnfav = () => {
+        const filterfav=favorite.filter((f)=>f.id!=recipe?.id)
+        Setfavorite(filterfav)
+        localStorage.setItem("fav",JSON.stringify(filterfav))
+    }
+
+    
+      useEffect(() => {
+        return () => {
         }
-    },[])
+
+    }, [favorite])
+ 
 
     return (
         recipe ? <div className='w-full flex'>
-            <div className="left w-1/2 p-2">
+            <div className="left w-1/2 p-10 relative">
+                {favorite.find((f)=>f.id==recipe?.id) ?(
+                    <i onClick={handleFav} className="ri-poker-hearts-line text-3xl text-red-500 absolute right-[5%]"></i>) :(
+                    <i onClick={handleUnfav} className="ri-poker-hearts-fill text-3xl text-red-500 absolute right-[5%]"></i>)
+                }
+
                 <h1 className='text-5xl font-black'>{recipe.title}</h1>
                 <img className='h-[20vh]' src={recipe.image} alt="" />
             </div>
